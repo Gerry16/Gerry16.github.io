71942cbd5111a435329b7bc868a1fc395518ad9d
Gerry16 <152454834+Gerry16@users.noreply.github.com>
2024-08-26 17:55:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+        function nuevoCliente() {
+            alert('Implementa lógica para nuevo cliente aquí.')
+        }
+
+        function entrar() {
+            alert('Implementa lógica para entrar aquí.')
+        }
+    </script>
